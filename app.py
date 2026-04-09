7ec849b4a9970cebdb2543eb82ddf46f8a4d57e5
jsymako <jsymako@gmail.com>
2026-04-09 16:38:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ with st.sidebar:
             "container": {"padding": "0!important", "background-color": "transparent", "border": "none"},
             "nav-link": {
                 "font-size": "1.40rem", "text-align": "left", "margin": "0px 0px 8px 0px",
-                "padding": "10px 6px", "border-radius": "8px", "border": "2px solid #7e7979", 
+                "padding": "10px 6px", "border-radius": "8px", "border": "2px solid #149aeb", 
                 "color": "#333", "font-weight": "normal"
             },
             "nav-link-hover": {"background-color": "#2b98d8", "border-color": "#2b98d8"},
