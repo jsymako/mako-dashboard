fc486d9652560d551291612638cf29fcdc9a9b40
jsymako <jsymako@gmail.com>
2026-05-18 18:25:23 +0900

    Update trade_trend.py

--- trade_trend.py
+++ trade_trend.py
@@ -9,7 +9,7 @@ def run(load_data_func):
     st.title("판매 현황")
     
     try:
-        with open("sales_trend.css", "r", encoding="utf-8") as f:
+        with open("style.css", "r", encoding="utf-8") as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     except FileNotFoundError:
         pass
